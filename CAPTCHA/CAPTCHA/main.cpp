05c69abc673402d03313ae415c2a08fc8912d699
Marin Jurlina <62703762+MarinXD5@users.noreply.github.com>
2021-10-13 14:22:57 +0200

    Add files via upload

--- CAPTCHA/CAPTCHA/main.cpp
+++ CAPTCHA/CAPTCHA/main.cpp
@@ -7,9 +7,8 @@ int sLength = sizeof(alpha) - 1;
 
 
 int main() {
-	cout << "Welcome to CAPTCHA v1.0. This CAPTCHA is only text based. You need to alphabetically sort five, five letter strings!" << endl << endl;
 	char captcha[10], userInput[10];
-	int string_length = 5;
+	int string_length = 5, cnt = 0;
 	srand(time(0));
 
 	for (int i = 0; i < string_length; i++) {
@@ -23,9 +22,17 @@ int main() {
 	cout << "Enter the CAPTCHA:";
 	cin >> userInput;
 
-	int r = strcmp(captcha, userInput);
+	for (int i = 0; i < string_length; i++) {
+		if (captcha[i] == userInput[i]){
+			cnt++;
+		}
+		else {
+			break;
+		}
+	}
+	
 
-	if (r) {
+	if (cnt==5) {
 		cout << "CAPTCHA MATCHED!!";
 	}
 	else {
