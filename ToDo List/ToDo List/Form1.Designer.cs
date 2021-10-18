130fd28a63ccdcdddbcd6f549d3404134ffc571d
Marin Jurlina <62703762+MarinXD5@users.noreply.github.com>
2021-10-18 23:34:23 +0200

    ToDo List v1.0
    Simple ToDo list that allows user to add or remove items from a text box.

---
+++ ToDo List/ToDo List/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace ToDo_List
+{
+    partial class ToDoList
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Remove = new System.Windows.Forms.Button();
+            this.Add = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.RemoveAll = new System.Windows.Forms.Button();
+            this.TaskText = new System.Windows.Forms.TextBox();
+            this.ListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // Remove
+            // 
+            this.Remove.Location = new System.Drawing.Point(600, 111);
+            this.Remove.Name = "Remove";
+            this.Remove.Size = new System.Drawing.Size(122, 48);
+            this.Remove.TabIndex = 1;
+            this.Remove.Text = "Remove";
+            this.Remove.UseVisualStyleBackColor = true;
+            this.Remove.Click += new System.EventHandler(this.Remove_Click);
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(600, 38);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(122, 48);
+            this.Add.TabIndex = 2;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(600, 362);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(122, 48);
+            this.Exit.TabIndex = 3;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // RemoveAll
+            // 
+            this.RemoveAll.Location = new System.Drawing.Point(600, 188);
+            this.RemoveAll.Name = "RemoveAll";
+            this.RemoveAll.Size = new System.Drawing.Size(122, 48);
+            this.RemoveAll.TabIndex = 5;
+            this.RemoveAll.Text = "Remove All";
+            this.RemoveAll.UseVisualStyleBackColor = true;
+            this.RemoveAll.Click += new System.EventHandler(this.RemoveAll_Click);
+            // 
+            // TaskText
+            // 
+            this.TaskText.Location = new System.Drawing.Point(24, 38);
+            this.TaskText.Name = "TaskText";
+            this.TaskText.Size = new System.Drawing.Size(488, 27);
+            this.TaskText.TabIndex = 8;
+            this.TaskText.TextChanged += new System.EventHandler(this.TaskText_TextChanged);
+            // 
+            // ListBox
+            // 
+            this.ListBox.FormattingEnabled = true;
+            this.ListBox.ItemHeight = 20;
+            this.ListBox.Location = new System.Drawing.Point(24, 186);
+            this.ListBox.Name = "ListBox";
+            this.ListBox.Size = new System.Drawing.Size(488, 224);
+            this.ListBox.TabIndex = 9;
+            this.ListBox.SelectedIndexChanged += new System.EventHandler(this.ListBox_SelectedIndexChanged);
+            // 
+            // ToDoList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.ClientSize = new System.Drawing.Size(800, 469);
+            this.Controls.Add(this.ListBox);
+            this.Controls.Add(this.TaskText);
+            this.Controls.Add(this.RemoveAll);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.Remove);
+            this.Name = "ToDoList";
+            this.Text = "To Do List v1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Remove;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button RemoveAll;
+        private System.Windows.Forms.TextBox TaskText;
+        private System.Windows.Forms.ListBox ListBox;
+    }
+}
+
---
+++ ToDo List/ToDo List/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDo_List
+{
+    public partial class ToDoList : Form
+    {
+        public ToDoList()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            ListBox.Items.Add(TaskText.Text.ToString());
+        }
+
+        private void RemoveAll_Click(object sender, EventArgs e)
+        {
+            ListBox.Items.Clear();
+        }
+
+        private void TaskText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Remove_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = ListBox.SelectedIndex;
+            ListBox.Items.RemoveAt(selectedIndex);
+        }
+        private void ListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
